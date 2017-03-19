f34c20525831f70c8e13b914f3e60c330def2d75
awygoda <wygoda.agnieszka@gmail.com>
2017-03-19 14:05:04 +0100

    Klasy User i Product

--- src/User.php
+++ src/User.php
@@ -8,5 +8,66 @@
  */
 class User
 {
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
 
 }
---
+++ tests/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kasona
+ * Date: 19.03.17
+ * Time: 13:50
+ */
+class UserTest extends \PHPUnit\Framework\TestCase
+{
+
+}
